76a9654d34c7294cd5b3da6b2d0f5786699d1b08
Camilo Salazar <camilo.salazar@cern.ch>
2017-08-06 14:46:47 +0200

    Real File added

--- LowPTMonoLeptonAnalyzer/CMSSW_8_0_25/src/LPTMLA/lptmla/python/ConfFile_cfg.py
+++ LowPTMonoLeptonAnalyzer/CMSSW_8_0_25/src/LPTMLA/lptmla/python/ConfFile_cfg.py
@@ -7,14 +7,15 @@ process.load("FWCore.MessageService.MessageLogger_cfi")
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.source = cms.Source("PoolSource",
-    # replace 'myfile.root' with the source file you want to use
-    fileNames = cms.untracked.vstring(
-        'file:myfile.root'
-    )
-)
+                            # replace 'myfile.root' with the source file you want to use
+                            fileNames = cms.untracked.vstring(
+        'root://cms-xrd-global.cern.ch//store/mc/RunIISummer16MiniAODv2/WJetsToLNu_HT-100To200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_Tranch\
+eIV_v6_ext1-v1/120000/14473FEF-1ACD-E611-8C84-00266CFFBC60.root'
+        )
+                            )
 
 process.demo = cms.EDAnalyzer('lptmla'
-)
+                              )
 
 
 process.p = cms.Path(process.demo)
